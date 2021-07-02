54a689edddcd5cc600ac934d2f6f8f03cbf64dc3
Andrew T. Smith <andrew.tombras.smith@gmail.com>
2021-07-02 07:20:06 -0700

    fixed plot

--- test_plot.py
+++ test_plot.py
@@ -3,12 +3,13 @@ import numpy as np
 
 
 def run():
-    x = np.linspace(0, np.pi*3.0, int(np.pi*3*10))
+    x = np.linspace(0, np.pi*2*2.0, int(np.pi*3*10))
     plt.plot(x, np.sin(x),'o-')
     plt.plot(x, np.cos(x),'o-')
     plt.legend(['sin(x)','cos(x)'])
     plt.ylabel('y')
     plt.xlabel('x')
+    plt.title("Test plot.")
     plt.show()
 
 
